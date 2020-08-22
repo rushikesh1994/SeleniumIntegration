cae44b5827e6d0b26652add9176d77f4700da7c4
Rushikesh Deshmukh <rdeshmukh@versionsolutions.com>
2020-08-23 03:32:52 +0530

    Add project files.

---
+++ SeleniumTestProject/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTestProject
+{
+    class Test
+    {
+        IWebDriver driver = new ChromeDriver();
+        [SetUp]
+        public void Setup()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        [Test]
+        public void Search()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("Testing" + Keys.Enter);
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
